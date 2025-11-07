1272cf9c16b31e6214a0d4e04800bc9ec2a31e9a
Athanasios Lazios <athanasioslazios0@gmail.com>
2025-11-07 19:51:18 +0100

    Zweiter Upload (Geheimzahl erraten C#)

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Geheimzahl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random zufall = new Random();
+            int geheimzahl = zufall.Next(1, 101); // Bis 100
+
+           Console.WriteLine("Erraten Sie die geheime Zahl!");
+           int benutzerversuch = Convert.ToInt32(Console.ReadLine());
+
+            int versuche = 1;
+
+            while (benutzerversuch != geheimzahl)
+            {
+                if (geheimzahl > benutzerversuch)
+                {
+                    Console.WriteLine("Zu niedrig!");
+                }
+                else
+                    {
+                    Console.WriteLine("Zu hoch!");
+                }
+                Console.WriteLine("Versuchen Sie es erneut!");
+                benutzerversuch = Convert.ToInt32(Console.ReadLine());
+                versuche++;
+            } 
+            Console.WriteLine("Geschafft!, Geheimzahl erraten.");
+            Console.WriteLine($"Sie haben {versuche} Versuche gebraucht.");
+        }
+    }
+}
